a707b810c345427662c08b7f1a3ace7f4ffa759c
MAbdulwassaykhan <120585055+MAbdulwassaykhan@users.noreply.github.com>
2025-05-12 17:29:30 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 1234
+let accountEmail ="Abdulwassay@gmail"
+var accountPassward ="102030"
+accountCity ="multan"
+
+//  accountId =505
+
+accountEmail="khan@gmail"
+accountPassward="1243"
+accountCity="lahore"
+
+// console.log(accountCity);
+
+console.table([accountId,accountEmail,accountPassward,accountCity])
