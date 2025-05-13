0c0d09bee6654125b07acb444ce146e725d75dff
MAbdulwassaykhan <120585055+MAbdulwassaykhan@users.noreply.github.com>
2025-05-13 15:42:46 +0000

    container config done

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,14 +1,50 @@
-const accountId = 1234
-let accountEmail ="Abdulwassay@gmail"
-var accountPassward ="102030"
-accountCity ="multan"
+// const accountId = 1234
+// let accountEmail ="Abdulwassay@gmail"
+// var accountPassward ="102030"
+// accountCity ="multan"
 
-//  accountId =505
+// //  accountId =505
 
-accountEmail="khan@gmail"
-accountPassward="1243"
-accountCity="lahore"
+// accountEmail="khan@gmail"
+// accountPassward="1243"
+// accountCity="lahore"
 
-// console.log(accountCity);
 
-console.table([accountId,accountEmail,accountPassward,accountCity])
+
+// console.table([accountId,accountEmail,accountPassward,accountCity])
+
+
+
+// let score = 123  // ==> number
+// let score = "123abc"  //==> number,NaN
+//let score = null   // ==> 0
+//let score = undefined  // ==> NaN
+//let score = true  // ==> 1,  false ==> 0
+// let score = "khan"
+
+
+// // const {score} = req.body
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+// let isLogedIn = "" //==> false
+// let isLogedIn = 1   //==>true
+// let isLogedIn = 0   //==>false
+// let isLogedIn ="khan"   //==>true
+
+// let booleanIsLogedIn = Boolean(isLogedIn)
+
+// console.log(booleanIsLogedIn);
+
+let somenumber = 234
+
+let stringNumber = String(somenumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
