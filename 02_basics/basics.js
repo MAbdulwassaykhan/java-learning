f97fc9e34188976b0d8de88f2a192a494b2ae52d
MAbdulwassaykhan <120585055+MAbdulwassaykhan@users.noreply.github.com>
2025-05-26 19:14:34 +0000

    array part2 done

--- 02_basics/basics.js
+++ 02_basics/basics.js
@@ -1,19 +1,19 @@
                                             //ARRAY//
 // Array part 1//
 
-const myArr = [0,1,2,3,4,5]
-const myHeros = ["thor","captain america","iron man"]
+// const myArr = [0,1,2,3,4,5]
+// const myHeros = ["thor","captain america","iron man"]
 
-const numbers = new Array(1,2,3,4,5)
+// const numbers = new Array(1,2,3,4,5)
 
-// console.log(myArr.length);
+// console.log(myArr.length);       // ==> 
 
 // Array methods//
 
-// myArr.push(5)
-// myArr.push(6)
-// myArr.push(7)
-// myArr.pop()
+// myArr.push(5)           // ==> [0,1,2,3,4,5,5]
+// myArr.push(6)           // ==> [0,1,2,3,4,5,5,6]
+// myArr.push(7)           // ==> [0,1,2,3,4,5,5,6,7]
+// myArr.pop()             // ==> [0,1,2,3,4,5,5,6,]
 
  
 // myArr.unshift(6)   // ==> put valus in start of array.
@@ -44,15 +44,45 @@ const numbers = new Array(1,2,3,4,5)
 
 // Arrar part 2//
 
-const marvel_heros = ["Thor","Iranman","Spiderman"]
-const dc_heros = ["Superman","Flash","Batman"]
+// const marvel_heros = ["Thor","Iranman","Spiderman"]
+// const dc_heros = ["Superman","Flash","Batman"]
+// marvel_heros.push(dc_heros) 
+// console.log(marvel_heros);           // ==> ["Thor","Iranman","Spiderman",["Superman","Flash","Batman"]]
+// console.log(marvel_heros.length);    // ==> 4
+// console.log(marvel_heros[3][1]);     // ==> Flash
 
-marvel_heros.push(dc_heros) 
-console.log(marvel_heros);
-console.log(marvel_heros.length);
-console.log(marvel_heros[3][1]);
 
+// const all_heros = marvel_heros.concat(dc_heros)      //   --|
+// console.log(all_heros);                              //     |  //  ==> [ 'Thor', 'Iranman', 'Spiderman', 'Superman', 'Flash', 'Batman' ]  
+                                                        //     |--->// both methods work same but second one is used the most
+// const all_new_heros = [...marvel_heros,...dc_heros]  //     |
+// console.log(all_new_heros);                          //   --|  //  ==> [ 'Thor', 'Iranman', 'Spiderman', 'Superman', 'Flash', 'Batman' ]
+
+
+// const anotherArr = [1,2,3,4,[5,6,7],8,9,10,[11,12,[13,14,15],16,17],18,19,20]
+// const real_anotherArr = anotherArr.flat(Infinity)
+// console.log(real_anotherArr);                              // ==> [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+
+
+// console.log(Array.isArray("Khan1234"));           // ==> false
+// console.log(Array.from("Khan1234"));              // ==> ['K','h','a','n','1','2','3','4']
+// console.log(Array.from({name:"Khan1234"}));       // ==> []
+
+
+
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+// console.log(Array.of(score1,score2,score3));          // ==> [100,200,300]
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                                            // OBJECTS //
 
+
+
+
+                                            
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
