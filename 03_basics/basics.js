f396a960be73e9b6d3cd1f4db4a615b18ed6ddf5
MAbdulwassaykhan <120585055+MAbdulwassaykhan@users.noreply.github.com>
2025-05-29 15:19:52 +0000

    arrow functions starting

--- 03_basics/basics.js
+++ 03_basics/basics.js
@@ -110,18 +110,21 @@
 // console.log(username);
 
 
-console.log(addOne(5));
-function addOne(num){
-    return num + 1
-}
+// console.log(addOne(5));
+// function addOne(num){
+//     return num + 1
+// }
 
 
 
-const addTwo = function(num){
-    return num + 2
-}
-console.log(addTwo(5));
+// const addTwo = function(num){
+//     return num + 2
+// }
+// console.log(addTwo(5));
 
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                                                     // THIS & ARROW FUNCTIONS //
 
 
 
