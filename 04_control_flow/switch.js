6e52f1cc552347f63d264fa80f7f286f5dde3f7d
MAbdulwassaykhan <120585055+MAbdulwassaykhan@users.noreply.github.com>
2025-05-30 18:52:36 +0000

    switch & truthy, falsy done

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -1,1 +1,45 @@
-// resume tomorrow from here.
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = "june"
+// switch (month) {
+//     case "jan":
+//         console.log("jan");
+        
+//         break;
+//     case "feb":
+//         console.log("feb");
+        
+//         break;
+//     case "march":
+//         console.log("march");
+        
+//         break;
+//     case "april":
+//         console.log("april");
+        
+//         break;
+//     case "may":
+//         console.log("may");
+        
+//         break;
+//     case "june":
+//         console.log("june");
+        
+//         break;
+
+//     default:
+//         console.log("default case match");
+        
+//         break;
+// }
+
+
+
+
---
+++ 04_control_flow/truthy_falsy.js
@@ -0,0 +1,49 @@
+// const userEmail = []
+// if (userEmail) {
+//     console.log("Got user email");
+    
+// } else {
+//     console.log("don't have user email");
+    
+// }
+
+// Falsy values//
+// false, 0,-0,BigInt 0n,"",null,undefined,NaN
+
+// Truthy values//
+// "0",'false," ",[],{},function(){}
+
+
+// if (userEmail.length === 0) {
+//     console.log("array is empty");
+    
+// }
+// const emptyObj ={}
+// if (Object.keys(emptyObj).length === 0) {
+//     console.log("object is empty");
+    
+// }
+
+// Nullish Coalescing operator (??): null undefined
+
+
+// let val1;
+// val1 = 5 ?? 15
+// val1 = null ?? 13
+// val1 = undefined ?? 12
+// val1 = null ?? 10 ?? 20 
+// val1 = null ?? undefined ?? 20 
+// val1 = null ?? undefined
+
+// console.log(val1);
+
+
+
+// Tirniary operator//
+
+// condition ? true : false 
+
+// const penPrice = 150
+// penPrice <= 160 ? console.log("greater than 140") : console.log("less then 140");
+
+
