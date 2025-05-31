13f288c3668ce26248300703e5f1b58968bb4f12
MAbdulwassaykhan <120585055+MAbdulwassaykhan@users.noreply.github.com>
2025-05-31 13:00:12 +0000

    six done

---
+++ 05_itrations/six.js
@@ -0,0 +1,17 @@
+
+
+// const coding = ["js","rb","py","swift","cpp"]
+
+// const languages = coding.forEach( (items)=>{
+//     // console.log(items);
+//     return items
+
+// } )
+// console.log(languages);
+
+const myNums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+const newNum = myNums.filter( (num)=> num > 8 )
+console.log(newNum);
+
+
